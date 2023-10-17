074205eb0ba94f21b8a355458dac400ad0fdda7f
Yang Zhang <yangzhang@live.com>
2023-10-17 21:45:56 +0800

    Initial commit

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/llvm/llvm-project/main/mlir/utils/emacs/mlir-mode.el
+wget https://raw.githubusercontent.com/llvm/llvm-project/main/llvm/utils/emacs/tablegen-mode.el
+wget https://raw.githubusercontent.com/llvm/llvm-project/main/llvm/utils/emacs/llvm-mode.el
